04b61b1a0401a3458ff057ce365f464145c45e6e
Jimmy Kivlen <jimmykivlen@gmail.com>
2020-05-15 03:15:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(dwarves)
   i = 0
-  array.each_with_index {|name, i| puts "#{i + 1}. #{name} "}
+  dwarves.each_with_index {|name, i| puts "#{i + 1}. #{name} "}
 end
 
 def summon_captain_planet(collection)
