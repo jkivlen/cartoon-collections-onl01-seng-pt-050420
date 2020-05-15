f228b440a8ca114b6355acd8c10a5ca4feb07b6f
Jimmy Kivlen <jimmykivlen@gmail.com>
2020-05-15 03:23:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,8 +11,9 @@ def summon_captain_planet(element)
   planeteer_calls
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  
+  planeteer 
 end
 
 def find_the_cheese# code an argument here
