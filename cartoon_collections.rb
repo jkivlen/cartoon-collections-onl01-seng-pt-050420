a6d9a4608cd72e4ae3067d41d8f6da00ed925ddc
Jimmy Kivlen <jimmykivlen@gmail.com>
2020-05-15 03:42:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,8 +18,8 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  cheese_types = ["cheddar", "gouda", "camembert"]
   array.find do |cheese|
   cheese.include(cheese_types) 
 end
+cheese_types = ["cheddar", "gouda", "camembert"]
 end
